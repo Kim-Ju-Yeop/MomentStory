dbc8d7af3b52075a24bb95a5da4c0ad1487da302
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2019-12-22 20:30:24 +0900

    Repository 정리

--- MomentStory_Server/util/cryptoUtil.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-const createHash = (password) => {
-    return crypto.createHash('sha512').update(password).digest('hex')
-}
-
-module.exports = {
-    createHash: createHash
-}
